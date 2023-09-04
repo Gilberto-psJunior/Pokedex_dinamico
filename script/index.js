95efe9322db28dbbb92a6b7509f6c56a119d4743
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-09-04 20:57:48 -0300

    init

---
+++ script/index.js
@@ -0,0 +1,25 @@
+const pokemonName = document.querySelector(".pokemon__name");
+const pokemonNumber = document.querySelector(".pokemon__number");
+const PokemonImg=document.querySelector(".pokemon__image");
+const pokemonType=document.querySelector(".pokemon__type");
+const fetchPokemon = async (pokemon) => {
+  const Response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+  const data = await Response.json();
+  return data;
+  
+};
+const renderPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon);
+  pokemonName.innerHTML = data.id;
+  pokemonName.innerHTML = data.name;
+  
+  PokemonImg.src=data['sprites']['versions']['generation-v'] ['black-white'] ['animated']['front_default']
+  
+  pokemonType.innerHTML=`Type:${data['types']['0']['type']['name']}`
+  console.log(data)
+};
+
+renderPokemon('3')
+
