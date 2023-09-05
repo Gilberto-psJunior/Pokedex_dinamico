e03c7002b294c13ed6190d7105e257cc4632534a
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-09-04 23:17:10 -0300

    last the night

--- script/index.js
+++ script/index.js
@@ -2,14 +2,17 @@ const pokemonName = document.querySelector(".pokemon__name");
 const pokemonNumber = document.querySelector(".pokemon__number");
 const PokemonImg=document.querySelector(".pokemon__image");
 const pokemonType=document.querySelector(".pokemon__type");
+const form=document.querySelector(".form");
+const input=document.querySelector(".input__search");
 const fetchPokemon = async (pokemon) => {
   const Response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-  );
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
   const data = await Response.json();
   return data;
   
 };
+
+
 const renderPokemon = async (pokemon) => {
   const data = await fetchPokemon(pokemon);
   pokemonName.innerHTML = data.name;
@@ -18,9 +21,18 @@ const renderPokemon = async (pokemon) => {
   
   PokemonImg.src=data['sprites']['versions']['generation-v'] ['black-white'] ['animated']['front_default']
   
-  pokemonType.innerHTML=`Type:${data['types']['0']['type']['name']}`
+  pokemonType.innerHTML=`Type: ${data['types']['0']['type']['name']}`
+
+  //adicionando a busca por nome ou numero da api
+  form.addEventListener('submit',async (event)=>{
+    event.preventDefault();
+    renderPokemon(input.value);
+    input.value='';
+  });
+  
+  
   console.log(data)
 };
 
-renderPokemon('1')
+renderPokemon('25')
 
