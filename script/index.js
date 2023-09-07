3f9c0e1ef4a6555968270e07d244e721ab96baed
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-09-06 22:37:06 -0300

    script botoes

--- script/index.js
+++ script/index.js
@@ -1,41 +1,50 @@
 const pokemonName = document.querySelector(".pokemon__name");
 const pokemonNumber = document.querySelector(".pokemon__number");
-const PokemonImg=document.querySelector(".pokemon__image");
-const pokemonType=document.querySelector(".pokemon__type");
-const form=document.querySelector(".form");
-const input=document.querySelector(".input__search");
+const PokemonImg = document.querySelector(".pokemon__image");
+const pokemonType = document.querySelector(".pokemon__type");
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input__search");
+const buton = document.querySelector(".btn-next");
+const buton2 = document.querySelector(".btn-prev");
+let searchPokemon = 1;
+
+
 const fetchPokemon = async (pokemon) => {
-  try {
-  const Response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+  const Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
   const data = await Response.json();
   return data;
-  } catch(err) {
-    console.log(err.response.data);
-  }
-  
 };
-
 
 const renderPokemon = async (pokemon) => {
-      const data = await fetchPokemon(pokemon);
-      pokemonName.innerHTML = data.species.name;
-      pokemonNumber.innerHTML = `#${data.id}`;
-      
-      
-      PokemonImg.src=data['sprites']['versions']['generation-v'] ['black-white'] ['animated']['front_default']
-      
-      pokemonType.innerHTML=`Type: ${data['types']['0']['type']['name']}`
-  
-      //adicionando a busca por nome ou numero da api
-      form.addEventListener('submit',async (event)=>{
-      event.preventDefault();
-      renderPokemon(input.value);
-      input.value='';
-      });
-      
-      
-      console.log(data)
-};
 
-renderPokemon('25')
+  const data = await fetchPokemon(pokemon);
+  pokemonName.innerHTML = data.species.name;
+  pokemonNumber.innerHTML = `#${data.id}`;
+  input.value = '';
+  searchPokemon = data.id;
+  PokemonImg.src =
+    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ];
+
+  pokemonType.innerHTML = `Type: ${data["types"]["0"]["type"]["name"]}`;
+};
+//adicionando a busca por nome ou numero da api
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+  input.value = "";
+});
+
+// adicionando botoes
+buton.addEventListener("click", () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+buton2.addEventListener("click", () => {
+  searchPokemon -= 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
